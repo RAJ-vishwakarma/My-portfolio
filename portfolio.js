f2b0edf4f0c5055c2db22c4fb257e0fef0548d85
jyoti <vishwakarsrv@gmail.com>
2022-06-16 15:14:39 +0530

    initial commit

--- portfolio.js
+++ portfolio.js
@@ -1,36 +1,41 @@
-$(document).scroll(function(){
-    var scroll = $(window).scrollTop();
-    var amount = -160+(scroll*0.8);
-    if (amount < 10)
-    {
-      $('.letter').css({left:amount+"px"});
-    }
-  });
+$(document).scroll(function () {
+  var scroll = $(window).scrollTop();
+  var amount = -160 + (scroll * 0.8);
+  if (amount < 10) {
+    $('.letter').css({ left: amount + "px" });
+  }
+});
 
-  $(document).scroll(function(){
-    var scroll = $(window).scrollTop();
-    var amount = -160+(scroll*0.8);
-    if (amount < 10)
-    {
-      $('.letter2').css({left:amount+"px"});
-    }
-  });
+$(document).scroll(function () {
+  var scroll = $(window).scrollTop();
+  var amount = -160 + (scroll * 0.8);
+  if (amount < 10) {
+    $('.letter2').css({ left: amount + "px" });
+  }
+});
 
-function openPopup(){
-  let popUp=document.getElementById('popup')
-  popUp.style.display= 'block';
+function openPopup() {
+  let popUp = document.getElementById('popup')
+  popUp.style.display = 'block';
 
-  
+
 }
 
-function closePopup(){
-  let popUp=document.getElementById('popup')
-  popUp.style.display= 'none';
+function closePopup() {
+  let popUp = document.getElementById('popup')
+  popUp.style.display = 'none';
 }
 
 
-function collectFormData(){
-var name= document.getElementById('name')
-console.log("name")
+function collectFormData(event) {
+  event.preventDefault();
+  var name = document.getElementById('name').value
+  console.log("name", name)
+
+  var email = document.getElementById('Email').value
+  console.log("Email", email)
+
+  var message = document.getElementById('Message').value
+  console.log("Message", message)
 
 }
